e2a5d75ac14009e8e038a9a982c824d03831bd6e
JM Udoy <junayedibne.udoy@gmail.com>
2023-08-18 11:57:17 +0600

    Creating Resources With POST Requests

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -31,7 +31,7 @@ class ProductController extends Controller
      */
     public function store(StoreProductRequest $request)
     {
-        //
+        return new ProductResource(Product::create($request->all()));
     }
 
     /**
